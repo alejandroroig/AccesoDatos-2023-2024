5d0c577c8dcd1a818b37fca18cce8e58b53a244b
alejandroroig <a.roigaguilar@edu.gva.es>
2023-09-09 02:15:34 +0200

    Añadido proyecto 1-1 colecciones

---
+++ UD01 Introduccion/P01-Colecciones/src/main/java/org/colecciones/entidades/BookCollection.java
@@ -0,0 +1,7 @@
+package org.colecciones.entidades;
+
+public class BookCollection {
+    // ¿Qué tipo de colección es la más adecuada para almacenar los libros?
+
+    // Crea los métodos solicitados en el enunciado del ejercicio
+}
